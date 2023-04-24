ce6c33c990fcaf39b49d9218a839338102421add
camillabutler <camilla_butler25@milton.edu>
2023-04-24 14:08:11 -0400

    4.24.23

--- src/myfarm.java
+++ src/myfarm.java
@@ -1,11 +1,27 @@
 public class myfarm {
     public static void main(String[] args) {
         myfarm charlie = new myfarm();
-
+        plot ryan = new plot();
+        ryan.printPlot();
 
     }
     public myfarm(){
         System.out.println("Hello from my amazing farm");
 
+        plot[][] grid = new plot[10][10];
+
+        for(int p=0; p < grid.length; p++) {
+            for (int h = 0; h < grid[p].length; h++) {
+                grid[p][h]=new plot();
+                grid[p][h].printPlot();
+
+            }
+        }
+
+
+
     }
+
+
 }
+
--- src/plot.java
+++ src/plot.java
@@ -2,20 +2,34 @@ public class plot {
     public String plantName;
     public int numberOfPlant;
     public boolean needsWater;
+
+
+
     public plot(){
       //  plantName = "basil";
-        needsWater = true;
+        needsWater = false;
         numberOfPlant =  (int)(Math.random() * 100) +10;
         int num = (int)(Math.random() *5);
         if (num == 0) {
             plantName = "sunflower";
         }
-            if (num == 1) {
-                plantName = "corn";
-            }
-            if (num == 2){
-                plantName = "basil";
+        if (num == 2){
+            plantName = "basil";
         }
-    }
+        if (num == 3){
+            plantName = "carrot";
+        }
+        if (num == 4){
+            plantName = "tomato";
+        }
+       else{
+           plantName = "empty";
+           numberOfPlant = 0;
+        }
 
+    }
+public void printPlot(){
+    System.out.println("the plot has " + numberOfPlant + "of "+ plantName+ "and it is " + needsWater + " that the plant needs water");
+
+}
 }
