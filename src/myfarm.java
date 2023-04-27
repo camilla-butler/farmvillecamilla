c3dfca1fafb64c9ab34a6ad3899d8de91f4a0548
camillabutler <camilla_butler25@milton.edu>
2023-04-27 17:21:27 -0400

    4.27.23

--- src/myfarm.java
+++ src/myfarm.java
@@ -4,23 +4,31 @@ public class myfarm {
         plot ryan = new plot();
         ryan.printPlot();
 
+
     }
-    public myfarm(){
+    public plot[][] grid;
+    public myfarm() {
         System.out.println("Hello from my amazing farm");
 
-        plot[][] grid = new plot[10][10];
+        grid = new plot[10][10];
 
-        for(int p=0; p < grid.length; p++) {
+        for (int p = 0; p < grid.length; p++) {
             for (int h = 0; h < grid[p].length; h++) {
-                grid[p][h]=new plot();
+                grid[p][h] = new plot();
                 grid[p][h].printPlot();
 
             }
         }
-
-
-
+        totalPlants();
     }
+
+
+        public void totalPlants() {
+        //how many plants are in the grid?
+        System.out.print("the total number of plants is " + grid);
+    }
+
+
 
 
 }
